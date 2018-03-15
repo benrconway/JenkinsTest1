e799116f3779651a445d567f33a238f14258c269
Ben Conway <ben.conway@bemo.co>
2018-03-15 10:44:57 +0000

    Add Jenkinsfile

--- server.sh
+++ server.sh
@@ -1,10 +1,39 @@
-#!/usr/bin/env sh
+{
+  agent any
 
-echo "Trying to build the project"
-set -x
+    environment {
+      CI = 'true'
 
-npm install
-  code/server/npm start &
+    }
+    stages {
 
+        stage('Build') {
+            steps {
+                dir('server'){
+                    git url: 'https://github.com/benrconway/JenkinsTest1.git'
+                    nodejs('testJS'){
+                        sh 'npm install'
+                    }
+                }
 
-set +x
+            }
+        }
+    #     stage('Test') {
+    #         steps {
+    #             dir('test'){
+    #                 git url: 'https://github.com/benrconway/JenkinsTest2.git'
+    #             nodejs('testJS') {
+    #                 sh 'test/npm install'
+    #                 sh 'test/mocha test.js'
+    #
+    #             }
+    #             echo 'Testing.. and stuff'
+    #             nodejs('testJS') {
+    #                 sh 'test/npm install'
+    #                 sh 'test/mocha test.js'
+    #             }
+    #         }
+    #     }
+    # }
+  }
+}
